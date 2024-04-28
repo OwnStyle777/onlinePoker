3ec3a3c5ab69ee385eb11a1d7d1953ed0a4ed48d
OwnStyle777 <OwnStyleXD@gmail.com>
2024-04-28 10:30:38 +0200

    create backend part of project in SpringBoot

---
+++ onlinePoker/src/main/java/com/example/onlinePoker/OnlinePokerApplication.java
@@ -0,0 +1,13 @@
+package com.example.onlinePoker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinePokerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinePokerApplication.class, args);
+	}
+
+}
---
+++ onlinePoker/src/test/java/com/example/onlinePoker/OnlinePokerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onlinePoker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinePokerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
