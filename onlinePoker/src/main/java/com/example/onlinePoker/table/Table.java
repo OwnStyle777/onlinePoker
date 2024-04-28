0233d1c84a0b50c5560c00ec8c93e4022b3e41c4
OwnStyle777 <OwnStyleXD@gmail.com>
2024-04-28 11:53:38 +0200

    create Table class add atributes and create getters and setters

--- onlinePoker/src/main/java/com/example/onlinePoker/table/Table.java
+++ onlinePoker/src/main/java/com/example/onlinePoker/table/Table.java
@@ -1,4 +1,42 @@
 package com.example.onlinePoker.table;
 
+
 public class Table {
+
+    private int pot;
+    private String[] flop;
+    private String turn;
+    private String river;
+
+    public int getPot() {
+        return pot;
+    }
+
+    public void setPot(int pot) {
+        this.pot = pot;
+    }
+
+    public String[] getFlop() {
+        return flop;
+    }
+
+    public void setFlop(String[] flop) {
+        this.flop = flop;
+    }
+
+    public String getTurn() {
+        return turn;
+    }
+
+    public void setTurn(String turn) {
+        this.turn = turn;
+    }
+
+    public String getRiver() {
+        return river;
+    }
+
+    public void setRiver(String river) {
+        this.river = river;
+    }
 }
