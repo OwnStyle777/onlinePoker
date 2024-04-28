85603eaaf990ef025aa871a5160416c5a0892e65
OwnStyle777 <OwnStyleXD@gmail.com>
2024-04-28 12:56:16 +0200

    create class DeckOfCards with constructor whicch initializes cards list and method which suffle cards

---
+++ onlinePoker/src/main/java/com/example/onlinePoker/table/DeckOFCards.java
@@ -0,0 +1,23 @@
+package com.example.onlinePoker.table;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class DeckOFCards {
+    private List<Card> cards ;
+
+    public DeckOFCards(){
+        initializeDeck();
+    }
+
+    private void initializeDeck(){
+        cards = new ArrayList<>();
+        cards.addAll(Arrays.asList(Card.values()));
+    }
+
+    private void shuffleCards(){
+        Collections.shuffle(cards);
+    }
+}
