9cabfa34437e2c4d7f74443b6755729fbdbd1367
OwnStyle777 <OwnStyleXD@gmail.com>
2024-04-28 11:16:36 +0200

    create Entity Player

--- onlinePoker/src/main/java/com/example/onlinePoker/onlinePokerApplication.java
+++ onlinePoker/src/main/java/com/example/onlinePoker/onlinePokerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OnlinePokerApplication {
+public class onlinePokerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OnlinePokerApplication.class, args);
+		SpringApplication.run(onlinePokerApplication.class, args);
 	}
 
 }
---
+++ onlinePoker/src/main/java/com/example/onlinePoker/players/Player.java
@@ -0,0 +1,4 @@
+package com.example.onlinePoker.players;
+
+public class Player {
+}
