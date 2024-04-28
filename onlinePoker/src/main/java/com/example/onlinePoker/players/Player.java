95c04dac4739cf30d868fc10caba94246ef4e786
OwnStyle777 <OwnStyleXD@gmail.com>
2024-04-28 11:44:39 +0200

    add som atributes for player class

--- onlinePoker/src/main/java/com/example/onlinePoker/players/Player.java
+++ onlinePoker/src/main/java/com/example/onlinePoker/players/Player.java
@@ -1,4 +1,45 @@
 package com.example.onlinePoker.players;
 
+import jakarta.persistence.Entity;
+
+@Entity
 public class Player {
+
+   private String name;
+   private int totalChips;
+   private String[] cards;
+   private String combination;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getTotalChips() {
+        return totalChips;
+    }
+
+    public void setTotalChips(int totalChips) {
+        this.totalChips = totalChips;
+    }
+
+    public String[] getCards() {
+        return cards;
+    }
+
+    public void setCards(String[] cards) {
+        this.cards = cards;
+    }
+
+    public String getCombination() {
+        return combination;
+    }
+
+    public void setCombination(String combination) {
+        this.combination = combination;
+    }
 }
---
+++ onlinePoker/src/main/java/com/example/onlinePoker/table/Table.java
@@ -0,0 +1,4 @@
+package com.example.onlinePoker.table;
+
+public class Table {
+}
