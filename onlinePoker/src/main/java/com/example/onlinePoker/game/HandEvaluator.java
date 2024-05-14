5e72ded00d1b71fe07ed6e522f899bfcd9d11efc
OwnStyle777 <OwnStyleXD@gmail.com>
2024-05-14 08:46:39 +0200

    create method which check for the pair, and method which sort the card by their value

--- onlinePoker/src/main/java/com/example/onlinePoker/game/HandEvaluator.java
+++ onlinePoker/src/main/java/com/example/onlinePoker/game/HandEvaluator.java
@@ -3,8 +3,37 @@ package com.example.onlinePoker.game;
 import com.example.onlinePoker.players.Player;
 import com.example.onlinePoker.table.Card;
 
+import java.util.*;
+
 public interface HandEvaluator {
-     default String checkHighestPair(Card[] playerCards, Card []cards){
+
+    default void sortTheCards(List <Card> cards){
+        cards.sort(Comparator.comparingInt(Card::getValue));
+    }
+     default String checkHighestPair(Card[] playerCards, Card [] tableCards){
+        List <Card> allCards = new ArrayList<>();
+
+        allCards.addAll(Arrays.asList(playerCards));
+        allCards.addAll(Arrays.asList(tableCards));
+        sortTheCards(allCards);
+
+
+        Card previousCard = null;
+        boolean isPair = false;
+
+        for (Card card: allCards){
+            if (previousCard != null && card.getValue() == previousCard.getValue()){
+                isPair = true;
+                break;
+
+            }
+            previousCard = card;
+        }
+
+    if (isPair){
+    return "is pair of " + previousCard.toString();
+       }
+
 return null;
     }
 }
