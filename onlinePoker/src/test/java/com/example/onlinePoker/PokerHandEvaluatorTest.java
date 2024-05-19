27c8e0c255a4b376eafe8b18e24882b569a03b15
OwnStyle777 <OwnStyleXD@gmail.com>
2024-05-19 14:52:00 +0200

    write parametized tests

---
+++ onlinePoker/src/test/java/com/example/onlinePoker/PokerHandEvaluatorTest.java
@@ -0,0 +1,58 @@
+package com.example.onlinePoker;
+
+import com.example.onlinePoker.game.HandEvaluator;
+import com.example.onlinePoker.table.Card;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class PokerHandEvaluatorTest implements HandEvaluator {
+
+
+    @ParameterizedTest
+    @MethodSource("provideCardsForStraightFlushTest")
+    void testCheckTheStraightFlush(Card[] playerCards, List<Card> tableCards, String expected) {
+        String result = checkTheStraightFlush(playerCards, tableCards);
+    assertEquals(expected, result);
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideCardsForStraightFlushTest")
+    void testCheckTheStraight(Card[] playerCards, List<Card> tableCards) {
+
+        String result = checkStraight(playerCards, tableCards);
+        assertEquals(" is Straight", result);
+    }
+
+    static Stream<Arguments> provideCardsForStraightFlushTest() {
+        return Stream.of(
+                Arguments.of(
+                        new Card[]{Card.SEVEN_CLUBS, Card.SIX_CLUBS},
+                        Arrays.asList(Card.FIVE_CLUBS, Card.FOUR_CLUBS, Card.THREE_CLUBS, Card.TWO_CLUBS, Card.A_DIAMONDS),
+                        "is Straight flush"
+                ),
+                Arguments.of(
+                        new Card[]{Card.TWO_CLUBS, Card.THREE_DIAMONDS},
+                        Arrays.asList(Card.FIVE_DIAMONDS, Card.SEVEN_DIAMONDS, Card.SIX_DIAMONDS, Card.EIGHT_DIAMONDS, Card.NINE_DIAMONDS),
+                        "is Straight flush"
+                ),
+                Arguments.of(
+                        new Card[]{Card.NINE_CLUBS, Card.TEN_CLUBS},
+                        Arrays.asList(Card.J_CLUBS, Card.Q_CLUBS, Card.K_CLUBS, Card.A_CLUBS, Card.TWO_DIAMONDS),
+                        "is Straight flush"
+                ),
+                Arguments.of(
+                        new Card[]{Card.A_HEARTS, Card.TWO_HEARTS},
+                        Arrays.asList(Card.FOUR_HEARTS, Card.Q_CLUBS, Card.FIVE_HEARTS, Card.A_CLUBS, Card.THREE_HEARTS),
+                        "is Straight flush"
+                )
+        );
+    }
+}
