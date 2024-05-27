0cb3b306864c76ba2d97095fc0bfd77ee2ebf8f0
OwnStyle777 <OwnStyleXD@gmail.com>
2024-05-27 15:59:55 +0200

    create class PlayerInfo data from Json

---
+++ onlinePoker/src/main/java/com/example/onlinePoker/players/PlayerInfo.java
@@ -0,0 +1,22 @@
+package com.example.onlinePoker.players;
+
+public class PlayerInfo {
+    String name;
+    int position;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void setPosition(int position) {
+        this.position = position;
+    }
+}
