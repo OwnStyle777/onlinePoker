b8302e471f5037f83b461a0bff8ce822d125a333
OwnStyle777 <OwnStyleXD@gmail.com>
2024-05-27 16:00:53 +0200

    allow request from localhost

--- onlinePoker/src/main/java/com/example/onlinePoker/api/WebSocketConfig.java
+++ onlinePoker/src/main/java/com/example/onlinePoker/api/WebSocketConfig.java
@@ -11,7 +11,9 @@ public class WebSocketConfig  implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOrigins("*").withSockJS();
+        registry.addEndpoint("/ws")
+                .setAllowedOrigins("http://localhost:1234")
+                .withSockJS();
     }
 
     @Override
