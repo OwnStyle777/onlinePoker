0e9a6965547687480a11450b1dc770bed5e0a678
OwnStyle777 <OwnStyleXD@gmail.com>
2024-06-02 14:47:12 +0200

    add div playerStatus to PlayerTable component and three conditions inside

--- reactpoker/src/components/PlayerTable.js
+++ reactpoker/src/components/PlayerTable.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import './PlayerTable.css';
 import { getCardImage } from '../utils/loadImages';
+import bigBlind from '../../public/images/bigBlind.png';
+import smallBlind from '../../public/images/smallBlind.png';
+import dealer from '../../public/images/dealer.png';
 
 
 
@@ -13,6 +16,11 @@ const PlayerTable = ({ player }) => {
                 {player.cards && player.cards.map((card, index) => (
                  <img  id="card1" key={index} src={getCardImage(`${card}.png`)} alt={card} />
                 ))}
+            </div>
+            <div className="playerStatus">
+                {player.bigBlind && <img src={bigBlind} className="blind" alt="bigBlind"/>}
+                {player.smallBlind && <img src={smallBlind} className="blind" alt = "smallBlind" />}
+                {player.dealer && <img src={dealer} className="blind" alt="dealer" />}
             </div>
         </div>
     );
