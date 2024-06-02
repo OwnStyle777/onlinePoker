60a142cb944d6d86df9bcc4b6ec221236b84be6d
OwnStyle777 <OwnStyleXD@gmail.com>
2024-06-02 13:35:04 +0200

    add calling of method getBlind to startGame method

--- onlinePoker/src/main/java/com/example/onlinePoker/api/PokerController.java
+++ onlinePoker/src/main/java/com/example/onlinePoker/api/PokerController.java
@@ -23,20 +23,23 @@ public class PokerController {
     @Autowired
     SimpMessagingTemplate messageTemplate;
 
-    private  List<Player> listOfPlayers = new ArrayList<>(Collections.nCopies(9, null));
-    Game game = new Game();
-    Table table = game.getTable();
+    private final List<Player> listOfPlayers = new ArrayList<>(Collections.nCopies(9, null));
+    private final Game game = new Game();
+    private final Table table = game.getTable();
+    private int smallBlind = 1;
+
 
     @MessageMapping("/getPlayers")
     @SendTo("/client/players")
     public List<Player> getPlayers() {
+
         return listOfPlayers;
     }
 
 
     @MessageMapping("/addPlayer")
-    @SendTo("/client/players")
-    public List<Player> addPlayer(@RequestParam PlayerInfo playerInfo) {
+    @SendTo("/client/game")
+    public Game addPlayer(@RequestParam PlayerInfo playerInfo) {
         String name = playerInfo.getName();
         int position = playerInfo.getPosition();
         System.out.println(name);
@@ -48,8 +51,11 @@ public class PokerController {
                 Player player = new Player();
                 player.setName(name);
                 listOfPlayers.set(position, player);
+                game.setPlayers(listOfPlayers);
+            }else {
+                throw new IllegalArgumentException("This place is occupied");
             }
-            return listOfPlayers;
+            return game;
 
         } else{
             throw new IllegalArgumentException("Player name cannot be null");
@@ -70,8 +76,14 @@ public class PokerController {
     @MessageMapping("/dealCards")
     public ResponseEntity<?> startGame() {
         if (checkPlayersNumber(listOfPlayers) >= 2) {
+
+            game.setPlayers(listOfPlayers);
             game.dealTheCards(listOfPlayers);
-            messageTemplate.convertAndSend("/client/players", listOfPlayers);
+            game.getBlinds(smallBlind, listOfPlayers);
+
+            System.out.println("deal the cards method");
+
+            messageTemplate.convertAndSend("/client/game", game);
             return ResponseEntity.ok().build();
         } else {
             return ResponseEntity.badRequest().body("Not enough players to start the game.");
@@ -81,7 +93,7 @@ public class PokerController {
     @SendTo("/client/cards")
     public List<Card>  dealTheFlop() {
 
-        System.out.println("method is called");
+        System.out.println("deal the flop method");
             game.dealTheFlop();
             List<Card> flop = table.getFlop();
         if(flop != null){
@@ -96,16 +108,31 @@ public class PokerController {
     @SendTo("/client/turn")
     public Card  dealTheTurn() {
 
-        System.out.println("method is called");
+        System.out.println("deal the turn method");
         game.dealTheTurn();
         Card turn = table.getTurn();
         if(turn != null){
             return turn;
         } else{
-            throw new IllegalArgumentException("Flop cannot be null");
+            throw new IllegalArgumentException("Turn cannot be null");
         }
-
     }
+
+    @MessageMapping("/dealTheRiver")
+    @SendTo("/client/river")
+    public Card  dealTheRiver() {
+
+        System.out.println("deal the river");
+        game.dealTheRiver();
+        Card river = table.getRiver();
+        if(river != null){
+            return river;
+        } else{
+            throw new IllegalArgumentException("River cannot be null");
+        }
+    }
+
+
 
 
 
